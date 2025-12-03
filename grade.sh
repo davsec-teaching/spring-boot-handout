22cf1e93d50eaafc3be80ec2030237b6e996f87b
Tapti Palit <tpalit@purdue.edu>
2025-12-03 15:48:47 -0800

    updated grade.sh

--- grade.sh
+++ grade.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# Assume redis is up and running
+cd persistence-framework
+mvn clean install
+# Insert mvn command to insert to local repository
+cd ..
+
+cd microservices
+mvn clean install
+mvn exec:java &
+cd ..
+
+sleep 20 # Wait for microservices to start
+
+cd main-app
+mvn clean install
+mvn exec:java
+cd ..
 
